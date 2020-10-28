dea5564f1d213a18b7eae124a32fa2ee3f84d29d
Gonzalo Rodríguez-Vila <gonzalorodriguez-vila@Gonzalos-MacBook-Pro.local>
2020-10-28 14:59:29 -0500

    adding Driver.java for prototype

---
+++ Driver.java
@@ -0,0 +1,13 @@
+import java.awt.event.*;
+
+//import model.*;
+//import controller.*;
+import view.*;
+
+public class Driver
+{
+    public static void main(String []args)
+    {
+        GameBoard gameTable = new GameBoard();
+    }
+}
