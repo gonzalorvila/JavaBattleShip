c211c782fdd8ad5d09f5b0ec4dc26fed7ff98ad2
Ryan Wilks <ryan.wilks@slu.edu>
2020-10-28 14:15:48 -0500

    Updating GridPanel

--- view/GridPanel.java
+++ view/GridPanel.java
@@ -8,23 +8,39 @@ public class GridPanel extends JPanel {
 
     private int length;
     private int height;
+    private JPanel grid;
     
-    public GridPanel(int length, int height) {
+    public GridPanel(String player, int length, int height) {
+        this.setBackground(Color.BLUE);
         this.length = length;
         this.height = height;
+        grid = new JPanel();
+        grid.setOpaque(false);
+        this.setLayout(new BorderLayout());
+        this.add(grid, BorderLayout.CENTER);
         this.setSize(length, height);
         this.setVisible(true);
+        
     }
 
-    public void paintGrid(Component c, Graphics g, int x, int y) {
+    public void Gridpaint(Graphics g) {
 
-        Graphics2D g2 = (Graphics2D)g;
+        int rows = 11;
+        int columns = 11;
+        int width = getSize().width;
+        int height = getSize().height;
 
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < height; j++) {
-                //g2.fillRect(x, y, this.width, this.height);
-                g2.drawRect(x, y, this.length, this.height);
-            }
+        //drawing the rows
+        int addRows = height / rows;
+        for (int i = 0; i < rows; i++) {
+            g.drawLine(0, i * addRows, width, i * addRows);
+        }
+
+        //drawing the columns
+        int addColumns = width / columns;
+        for (int i = 0; i < columns; i++) {
+            g.drawLine(i * addColumns, 0, i * addColumns, height);
         }
     }
+
 }
