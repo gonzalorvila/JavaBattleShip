b6a91e0a81f78728142b234666f9ae7a4f681888
Gonzalo Rodríguez-Vila <gonzalorodriguez-vila@Gonzalos-MacBook-Pro.local>
2020-11-04 12:28:52 -0600

    Adding Opponent class

---
+++ model/Opponent.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Opponent extends Player
+{
+    public Opponent()
+    {
+        System.out.println("Opponent constructor");
+    }
+
+    public void opponentMove()
+    {
+        System.out.println("Opponent:: opponentMove will move for Opponent")
+    }
+}
