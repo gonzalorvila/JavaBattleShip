28c1fc0dfbbb5ee87336fd5c52914a7e54bd2f60
Gonzalo Rodríguez-Vila <gonzalorodriguez-vila@Gonzalos-MacBook-Pro.local>
2020-11-06 11:44:33 -0600

    Small change to player and ships

--- model/Player.java
+++ model/Player.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 
 public class Player {
 	private Ships ship;
-	private GameBoardState gameBoardState;
 	
 	public Player() {
 		System.out.println("Player Constructor");
--- model/Ships.java
+++ model/Ships.java
@@ -3,7 +3,6 @@ package model;
 import java.util.ArrayList;
 
 public class Ships {
-	private GameBoardState gameBoardState;
 	private String shipName;
 	private int length;
 	private int location;
