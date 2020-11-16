dd4fe11411d2dca05daacc372ce6578aaa88f3b2
Gonzalo Rodríguez-Vila <gonzalorodriguez-vila@Gonzalos-MacBook-Pro.local>
2020-11-16 17:48:52 -0600

    Made BattleShipUserInterface

---
+++ controller/BattleShipUserInterface.java
@@ -0,0 +1,16 @@
+package controller;
+import model.*;
+import view.*;
+import java.awt.event.ActionListener;
+
+public interface BattleShipUserInterface
+{
+    public void createGameBoard(ActionListener shipActionListener, ActionListener gridActionListener);
+    public void showValidShipPlacements(GridButton selectedButton);
+    public Ships placeShip(GridButton secondButton);
+    public void enableUserGrid(boolean enabled);
+    public void enableComputerGrid(boolean enabled);
+    public void setSelectedShip(ShipButton ship);
+    public ShipPanel getShipPanel();
+    public void setMessage(String message);
+}
