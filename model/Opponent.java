47e2699810cd836263d1b84dbe90964e16d5cd71
Caleb J. Potter <caleb.potter@slu.edu>
2020-11-17 17:20:02 -0600

    Update Opponent.java

--- model/Opponent.java
+++ model/Opponent.java
@@ -5,47 +5,6 @@ import java.util.ArrayList;
 
 public class Opponent
 {
-	private int rowGuess;
-	private int columnGuess;
-	private ArrayList prevMoves; // = new ArrayList();
-	private ArrayList<Boolean> moveResults; // = new ArrayList();
-	private int numOfMoves = 0;
-	private boolean result;
-
-	private boolean direction; // if this is true then the ship will be placed vertically otherwise it will be horizontal
-	private ArrayList<Ships> userShipLocations;
-
-
-
-	public Opponent()
-	{
-        	System.out.println("Opponent constructor");
-    	}
-
-    	public int opponentMove()
-    	{
-		/*if (prevMoves.isEmpty() {
-
-			Random num = new Random();
-        		oppGuess = num.nextInt(101);
-			prevMoves.add(oppGuess);
-			result = checkOppMove(oppGuess);
-			moveResults.add(result);
-		} else {
-			if (moveResults.get(numOfMoves -1) {
-				oppGuess = prevMoves.get(moves - 1) + 1;
-				if (oppGuess < 101) {
-					prevMoves.add(oppGuess)
-				} else {
-					oppGuess = prevMoves.get(moves - 1) - 1;*/
-		System.out.println("Opp move");
-		return 0;
-    	}
-
-	//public boolean checkOppMove(int oppMove) {
-		
-}
-
 	private ArrayList<Integer> locations;
 	private GameBoardState gbs;
 	private int[] shipLengths;
