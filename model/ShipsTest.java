aafc0ef5f780c3287895ad004ee33811cfb520b7
Ryan Wilks <ryan.wilks@slu.edu>
2020-11-19 23:26:46 -0600

    Updated ShipsTest

--- model/ShipsTest.java
+++ model/ShipsTest.java
@@ -7,31 +7,62 @@ import java.util.ArrayList;
 public class ShipsTest {
     
     @Test
+    public void checkStartColumn() {
+        Ships ship = new Ships(4, 7, 6, 7, 3);
+        int result = ship.getStartColumn();
+        assertEquals(7, result);
+    }    
+
+    @Test
+    public void checkStartRow() {
+        Ships ship = new Ships(4, 7, 6, 7, 3);
+        int result = ship.getStartRow();
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void checkEndColumn() {
+        Ships ship = new Ships(4, 7, 6, 7, 3);
+        int result = ship.getEndColumn();
+        assertEquals(7, result);
+    }
+
+    @Test
+    public void checkEndRow() {
+        Ships ship = new Ships(4, 7, 6, 7, 3);
+        int result = ship.getEndRow();
+        assertEquals(6, result);
+    }
+
+    @Test
     public void checkLength() {
-        Ships ship = new Ships(3, 23, 25, true);
-        int result = ship.getLength();
+        Ships ship = new Ships(4, 7, 6, 7, 3);
+        int result = ship.getShipLength();
         assertEquals(3, result);
     }
 
     @Test
-    public void checkStartLocation() {
-        Ships ship = new Ships(3, 23, 25, true);
-        int result = ship.getStartLocation();
-        assertEquals(23, result);
+    public void checkScore() {
+        Ships ship = new Ships(4, 7, 6, 7, 3);
+        ship.setScore(5);
+        int result = ship.getScore();
+        assertEquals(5, result);
     }
 
     @Test
-    public void checkEndLocation() {
-        Ships ship = new Ships(3, 23, 25, true);
-        int result = ship.getEndLocation();
-        assertEquals(25, result);
+    public void checkStoringRowsFilled() {
+        Ships ship = new Ships(4, 7, 6, 7, 3);
+        int[] rowsFilled = ship.StoringRowsFilled();
+        int result = rowsFilled.length;
+        assertEquals(3, result);
     }
 
     @Test
-    public void checkDirection() {
-        Ships ship = new Ships(3, 23, 25, true);
-        boolean result = ship.getDirection();
-        assertEquals(true, result);
+    public void checkStoringColumnsFilled() {
+        Ships ship = new Ships(4, 7, 6, 7, 3);
+        int[] columnsFilled = ship.storingColumnsFilled();
+        int result = columnsFilled.length;
+        assertEquals(3, result);
     }
 
 }
